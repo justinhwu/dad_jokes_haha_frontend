e6d77a8d29fdfc676c09d64ec23fa0c5a8520890
justinhwu <48028182+justinhwu@users.noreply.github.com>
2019-05-27 21:20:41 -0400

    Revert "Able to post random jokes"

--- src/joke.js
+++ src/joke.js
@@ -1,36 +1,3 @@
 class Joke{
   //how do I incorporate the api here?
-
-  constructor(jokeObj){
-    this.id = jokeObj.id
-    this.phrase = jokeObj.joke
-  }
-
-  static randJoke(){
-    fetch(randomJoke,{
-      method: 'get',
-      headers: {
-        'User-Agent': 'My Library (https://github.com/justinhwu/dad_jokes_haha_frontend)',
-        'Accept': 'application/json'
-      }
-    })
-    .then(resp => resp.json())
-    .then(jokeObj => {
-      let newJoke = new Joke(jokeObj)
-      newJoke.render()
-    })
-  }
-
-  render(){
-    let paragraph = document.createElement('p')
-    let randomJokeDiv = document.getElementById('random-joke-div')
-    paragraph.dataset.jokeId = this.id
-    paragraph.innerText = this.phrase
-    randomJokeDiv.appendChild(paragraph)
-  }
-
-
-
-
-
 }
