bc85cde21c19860376fb0bc938e256fd633518e9
Justin <justinhwu95@gmail.com>
2019-05-28 10:59:31 -0400

    Created a modal for the login button

--- src/index.js
+++ src/index.js
@@ -1,14 +1,50 @@
 let rand_div = document.getElementById('random-joke-div')
 let joke_button = document.getElementById('random-joke-button')
+let add_to_list_button = document.getElementById('add-joke-to-list')
 
 
 document.addEventListener('DOMContentLoaded', ()=>{
 //still needs callback
-Joke.randJoke()
-joke_button.addEventListener('click', (e)=> {
-  let paragraph = document.querySelector('p')
-  e.preventDefault()
-  rand_div.removeChild(paragraph)
-  Joke.randJoke()
-  })
+    let user = undefined
+    Joke.randJoke()
+    joke_button.addEventListener('click', (e)=> {
+      let paragraph = document.querySelector('p')
+      e.preventDefault()
+      rand_div.removeChild(paragraph)
+      Joke.randJoke()
+    })
+
+    loginModal(user)
 })
+
+
+function loginModal(user){
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks the button, open the modal
+  btn.onclick = function() {
+    if (user !== undefined){
+      console.log('Hi')
+    }
+    else{
+      modal.style.display = "block";
+    }
+  }
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
