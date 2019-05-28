1b2d98edcaa872f4e018f519c3525855ef924f42
Justin <justinhwu95@gmail.com>
2019-05-27 22:23:38 -0400

    Can post random jokes from API to webpage

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-document.addEventListener('DOMContentLoaded'){
-//still needs callback
-let joke_button = document.getElementById('random-joke-button')
 let rand_div = document.getElementById('random-joke-div')
+let joke_button = document.getElementById('random-joke-button')
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+//still needs callback
 Joke.randJoke()
 joke_button.addEventListener('click', (e)=> {
   let paragraph = document.querySelector('p')
   e.preventDefault()
   rand_div.removeChild(paragraph)
-
   Joke.randJoke()
   })
 })
