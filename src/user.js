df3ee37e906f9cb6e3ff68036664d595dfcc2771
github username <github email address>
2019-05-28 16:28:15 -0400

    merge to master

--- src/user.js
+++ src/user.js
@@ -12,5 +12,17 @@ class User {
     let addToListActionDiv = document.getElementById('add-to-list-div')
 
     let loggedInListsDiv = document.getElementById('logged-in-lists-div')
+
+    // <div class="ui middle aligned center aligned grid">
+    //   <div class="column">
+    //     <h2 class='ui teal image header'>
+    //       <img src="https://mickeygomez.com/wp-content/uploads/2012/06/dadrem.jpg?w=296" class="image" alt="dad-smile">
+    //       <div class="content">Log in to Dad Jokes</div>
+    //     </h2>
+    //
+    //   </div>
+    // </div>
+
+
   }
 }
