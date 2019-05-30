fe11a2554198ab0bb43157ee8b88f5687bf62019
Justin <justinhwu95@gmail.com>
2019-05-29 22:45:19 -0400

    updated index to reflect OO

--- src/index.js
+++ src/index.js
@@ -40,48 +40,15 @@ function initLoginModal(){
 
   // When the user clicks the login icon top-right, open the modal
   loginIcon.onclick = function () {
-    if (user !== undefined){
-      //hides the login form
-
-      login_form.style.display = 'none'
-      modal.style.display = "block"
-      if (checkbox_div.childNodes.length <= 1 ){
-      //If the checkbox elements do not exist, then the program will display the checkbox once upon click
-        let create_new_list_button = document.createElement('button')
-        create_new_list_button.id = 'create_new_list_button'
-        create_new_list_button.className = 'ui teal button'
-        create_new_list_button.innerText = 'Create a New List'
-        let add_to_existing_list_button = document.createElement('button')
-        add_to_existing_list_button.id = 'add_to_existing_list_button'
-        add_to_existing_list_button.className = 'ui orange button'
-        add_to_existing_list_button.innerText = 'Add to an Existing List'
-
-        modal_content.append(create_new_list_button, add_to_existing_list_button)
-        add_to_existing_list_button.addEventListener('click', displayLists)
-        create_new_list_button.addEventListener('click', displayNewListForm)
-      }
-    }
-    else{
+    if(User.all.length === 0){
       modal.style.display = "block";
       let usernameVal = document.getElementById('username')
       login_form.addEventListener('submit', (e)=>{
         e.preventDefault()
-        fetch(loginRailsApi, {
-          method: 'post',
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-          }
-          ,
-          body: JSON.stringify({
-            'username': usernameVal.value
-          })
-        })
-        .then(resp=> resp.json())
-        .then(userObj => {handleLogin(userObj)})
+        User.fetchUser()
       })
+      }
     }
-  }
   // When the user clicks on <span> (x), close the modal
   span.onclick = function() {
     modal.style.display = "none";
@@ -114,7 +81,7 @@ function initLoginModal(){
         add_to_existing_list_button.innerText = 'Add to an Existing List'
 
         modal_content.append(create_new_list_button, add_to_existing_list_button)
-        add_to_existing_list_button.addEventListener('click', displayLists)
+        add_to_existing_list_button.addEventListener('click', List.displayLists)
         create_new_list_button.addEventListener('click', displayNewListForm)
       }
     }
@@ -139,76 +106,18 @@ function initLoginModal(){
   }
 }
 
-
-
-function displayLists(){
-  if (checkbox_div.childNodes.length <= 1 ){
-
-  let joke = document.querySelector(`p`)
-
-  //creates prompt for user to choose a list to add to
-  let add_to_list_header = document.createElement('h2')
-  add_to_list_header.innerText = 'Please choose a list to add this joke to!'
-  //creates form to submit
-  let add_to_list_form = document.createElement('form')
-  let unordered_list = document.createElement('ul')
-  let submit = document.createElement('input')
-  submit.type = 'submit'
-  submit.value = 'Add to List'
-  add_to_list_form.id = 'add_to_list_form'
-
-  //Creates checkbox with each list item
-  user.lists.forEach((list)=> {
-    let list_element = document.createElement('li')
-    let list_header = document.createElement('header')
-    list_header.innerText = list.name
-    let list_box = document.createElement('input')
-    list_box.id = list.id
-    list_box.type = 'checkbox'
-    list_element.append(list_header, list_box)
-    unordered_list.appendChild(list_element)
-  })
-  add_to_list_form.append(unordered_list, submit)
-  checkbox_div.append(add_to_list_header, add_to_list_form)
-  }
-}
-
 function displayNewListForm(){
-
-  let add_to_list_form = document.createElement('form')
-
-    add_to_list_form.addEventListener('submit', (e)=>{
-      e.preventDefault()
-      let checkbox = document.querySelectorAll('.check-box')
-      let checked_ids = []
-      checkbox.forEach((element)=>{
-        if (element.checked){
-          checked_ids.push(element.id)
-        }
-      })
-      let joke_id = document.querySelector('p')
-      fetch(`http://localhost:3000/jokes`, {
-        method:'post',
-        headers:{
-          'Content-Type': 'application/json',
-          Accept: 'application/json'
-        },
-        body: JSON.stringify({
-          'id': joke_id.dataset.jokeId,
-          'joke': joke_id.innerText,
-          'list_ids': checked_ids
-        })
-      })
-
-    })
-  let name_label = document.createElement('label')
-  name_label.innerText = 'Name: '
-  let name_input = document.createElement('input')
-  name_input.type = 'text'
-  let submit = document.createElement('input')
-  submit.type = 'submit'
-  submit.value = 'Create New List'
-  add_to_list_form.append(name_label, name_input, submit)
-  modal_content.prepend(add_to_list_form)
-
-}
+  // if (checkbox_div.childNodes.length <= 1 ){
+  // // let add_to_list_form = document.createElement('form')
+  // //
+  // //
+  // // let name_label = document.createElement('label')
+  // // name_label.innerText = 'Name: '
+  // // let name_input = document.createElement('input')
+  // // name_input.type = 'text'
+  // // let submit = document.createElement('input')
+  // // submit.type = 'submit'
+  // // submit.value = 'Create New List'
+  // // add_to_list_form.append(name_label, name_input, submit)
+  // // modal_content.prepend(add_to_list_form)
+  }
--- src/list.js
+++ src/list.js
@@ -7,6 +7,67 @@ class List {
     List.all.push(this)
   }
   static all = []
+
+  static displayLists(){
+    if (checkbox_div.childNodes.length <= 1 ){
+    let joke = document.querySelector(`p`)
+
+    //creates prompt for user to choose a list to add to
+    let add_to_list_header = document.createElement('h2')
+    add_to_list_header.innerText = 'Please choose a list to add this joke to!'
+    //creates form to submit
+    let add_to_list_form = document.createElement('form')
+    let unordered_list = document.createElement('ul')
+    let submit = document.createElement('input')
+    submit.type = 'submit'
+    submit.value = 'Add to List'
+    add_to_list_form.id = 'add_to_list_form'
+
+    add_to_list_form.addEventListener('submit', (e)=>{
+      e.preventDefault()
+      let checkbox = document.querySelectorAll('.check-box')
+      let checked_ids = []
+
+      checkbox.forEach((element)=>{
+        if (element.checked){
+          debugger
+          checked_ids.push(element.id)
+        }
+      })
+
+      let joke_id = document.querySelector('p')
+      fetch(`http://localhost:3000/jokes`, {
+        method:'post',
+        headers:{
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        },
+        body: JSON.stringify({
+          'id': joke_id.dataset.jokeId,
+          'joke': joke_id.innerText,
+          'list_ids': checked_ids
+        })
+      })
+
+    })
+
+    //Creates checkbox with each list item
+    List.all.forEach((list)=> {
+      let list_element = document.createElement('li')
+      list_element.className = 'checkbox-list-select'
+      let list_header = document.createElement('header')
+      list_header.innerText = list.name
+      let list_box = document.createElement('input')
+      list_box.id = list.id
+      list_box.type = 'checkbox'
+      list_element.append(list_header, list_box)
+      unordered_list.appendChild(list_element)
+    })
+    add_to_list_form.append(unordered_list, submit)
+    checkbox_div.append(add_to_list_header, add_to_list_form)
+    }
+  }
+
   // static getUserLists(){
   //   fetch(`http://localhost:3000.com/users/${user.id}`)
   //   .then(res => res.json())
